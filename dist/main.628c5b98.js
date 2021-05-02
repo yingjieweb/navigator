742318662c7b57bbee88430db40055cb47d362f9
yingjieweb <1184061078@qq.com>
2021-05-02 22:55:43 +0800

    basic optimize addSite icon diff func

--- dist/main.628c5b98.js
+++ dist/main.628c5b98.js
@@ -127,6 +127,16 @@ module.exports = "icon3.b006812b.png";
 module.exports = "icon4.c8dee8ec.png";
 },{}],"f9I4":[function(require,module,exports) {
 module.exports = "icon5.b6d93138.png";
+},{}],"TLFA":[function(require,module,exports) {
+module.exports = "icon6.e05a3c92.png";
+},{}],"aS9c":[function(require,module,exports) {
+module.exports = "icon7.20914dd8.png";
+},{}],"EsZT":[function(require,module,exports) {
+module.exports = "icon8.1688a462.png";
+},{}],"WTB7":[function(require,module,exports) {
+module.exports = "icon9.791bc191.png";
+},{}],"jYai":[function(require,module,exports) {
+module.exports = "icon10.e22aac8a.png";
 },{}],"kAkt":[function(require,module,exports) {
 module.exports = "yourname.7afbc98d.jpg";
 },{}],"UonK":[function(require,module,exports) {
@@ -173,19 +183,19 @@ var $indicatorUl = $('.indicator ul'); // 切换标识 ul
 
 var $indicatorLis = $indicatorUl.find('li'); // 切换标识 ul > lis
 
-var $notesInput = $('.notesInput'); // 获取便签input
+var $notesInput = $('.notesInput'); // 获取便签 input
 
-var $notesList = $('.notesList'); // 获取便签List
+var $notesList = $('.notesList'); // 获取便签 list
 
-var $notesListUl = $('.notesListUl'); // 获取便签List
+var $notesListUl = $('.notesListUl'); // 获取便签 list
 
 var $notesButton = $('.notesButton'); // 获取便签按钮
 
-var $tabBar = $('.tab-bar'); // 获取tabBar的按钮
+var $tabBar = $('.tab-bar'); // 获取 tabBar 的按钮
 
-var $search = $('.search'); // 获取search表单
+var $search = $('.search'); // 获取 search 表单
 
-var $input = $('.search input'); // 获取search表单的input
+var $input = $('.search input'); // 获取 search 表单的 input
 
 var $addSiteLi = $('.addSiteLi'); // 获取新增快捷方式按钮
 
@@ -202,7 +212,7 @@ $notesButton.on('click', function () {
   if ($notesInput.val() === '') {
     alert('你还没写愿望呢，小傻瓜！');
   } else {
-    $notesListUl.append("\n      <li>\n          <span style=\"line-height: 40px; max-width: 280px;\">".concat($notesInput.val(), "</span>\n          <svg class=\"icon\"><use xlink:href=\"#icon-aixin\"></use></svg>\n        </li>\n    "));
+    $notesListUl.append("\n      <li>\n          <span style=\"line-height: 40px max-width: 280px\">".concat($notesInput.val(), "</span>\n          <svg class=\"icon\"><use xlink:href=\"#icon-aixin\"></use></svg>\n        </li>\n    "));
     notesArray.push($notesInput.val());
 
     if (notesArray.length > 1 && notesArray[0] === '您目前还没有梦想呢，小傻瓜 ~') {
@@ -225,10 +235,10 @@ $tabBar.on('click', "div", function (event) {
   // tabBar事件委托
   var $tabItem = $(event.currentTarget); //获取当前被点击的元素
 
-  $tabItem.addClass("selected").siblings().removeClass("selected"); //toggleClass(value,stateVal); 看mdn吧
+  $tabItem.addClass("selected").siblings().removeClass("selected"); // toggleClass(value, stateVal) link mdn
 
-  var index = $tabItem.index(); //采取四个表单的措施也是可以的，或者采用判断 index 值修改表单的action和input的name属性
-  //$tabContent.children().eq(index).addClass("active").siblings().removeClass("active");
+  var index = $tabItem.index(); // 采取四个表单的措施也是可以的，或者采用判断 index 值修改表单的action和input的name属性
+  // $tabContent.children().eq(index).addClass("active").siblings().removeClass("active")
 
   if (index === 0) {
     $search.attr("action", "http://www.baidu.com/s");
@@ -247,17 +257,17 @@ $tabBar.on('click', "div", function (event) {
     $input.attr("name", "q");
     $input.attr("placeholder", "创建自己的开源项目 — GitHub");
   } else if (index === 4) {
-    $search.attr("action", "http://ss.chaoxing.com/search"); //超新星
+    $search.attr("action", "http://ss.chaoxing.com/search"); // 超新星
 
     $input.attr("name", "sw");
     $input.attr("placeholder", "超星发现 —— 小傻瓜加油！");
   } else if (index === 5) {
-    $search.attr("action", "http://xueshu.baidu.com/s"); //百度学术
+    $search.attr("action", "http://xueshu.baidu.com/s"); // 百度学术
 
     $input.attr("name", "wd");
     $input.attr("placeholder", "保持学习的态度 —— 百度学术");
   } else {
-    $search.attr("action", "https://book.duxiu.com/search"); //读秀
+    $search.attr("action", "https://book.duxiu.com/search"); // 读秀
 
     $input.attr("name", "sw");
     $input.attr("placeholder", "海量学术文献搜索 —— 读秀");
@@ -265,22 +275,24 @@ $tabBar.on('click', "div", function (event) {
 }); // 页面渲染render
 
 var render = function render() {
-  $('.siteList').find('li:not(.addSiteLi)').remove(); //渲染前移除添加按钮前的模块
+  $('.siteList').find('li:not(.addSiteLi)').remove(); // 渲染前移除添加按钮前的模块
 
   hashMap.forEach(function (item, index) {
-    //根据hashMap创建相应的元素并添加到新增按钮前
+    // 根据hashMap创建相应的元素并添加到新增按钮前
     var $li = $("<li class=\"block\">\n      <a href=\"".concat(item.url, "\">\n        <div class=\"logo\"><img src=").concat(item.logoPath, " alt=\"\u70B9\u51FB\u6DFB\u52A0\"></div>\n        <div class=\"site\">").concat(simplifyUrl(item.url), "</div>\n      </a>\n      <div class=\"close\">\xD7</div>\n    </li>\n    ")).insertBefore($addSiteLi);
+    if (hashMap.length >= 10) $addSiteLi.css('visibility', 'hidden');
     $li.on('click', '.close', function (event) {
-      event.stopPropagation(); //阻止事件冒泡
+      event.stopPropagation(); // 阻止事件冒泡
 
       hashMap.splice(index, 1);
+      $addSiteLi.css('visibility', 'visible');
       render();
     });
   });
   $notesListUl.find('li').remove(); //渲染前移除之前的notes
 
   notesArray.forEach(function (item) {
-    var $li = $("<li title=\"\u70B9\u51FB\u5C0F\u7EA2\u5FC3\u5C31\u8868\u793A\u613F\u671B\u5DF2\u7ECF\u5B9E\u73B0\u4E86\u54E6 ~\">\n      <span style=\"line-height: 40px; max-width: 280px;\">".concat(item, "</span>\n      <svg class=\"icon\" onclick=\"window.alert('\u613F\u671B\u5B9E\u73B0\u529F\u80FD\u6B63\u5728\u5F00\u53D1\u5F53\u4E2D\u5462 ~')\"><use xlink:href=\"#icon-aixin\"></use></svg>\n    </li>\n    "));
+    var $li = $("<li title=\"\u70B9\u51FB\u5C0F\u7EA2\u5FC3\u5C31\u8868\u793A\u613F\u671B\u5DF2\u7ECF\u5B9E\u73B0\u4E86\u54E6 ~\">\n      <span style=\"line-height: 40px max-width: 280px\">".concat(item, "</span>\n      <svg class=\"icon\" onclick=\"window.alert('\u613F\u671B\u5B9E\u73B0\u529F\u80FD\u6B63\u5728\u5F00\u53D1\u5F53\u4E2D\u5462 ~')\"><use xlink:href=\"#icon-aixin\"></use></svg>\n    </li>\n    "));
     $notesListUl.append($li);
   });
 }; // 页面刷新时先渲染 hashMap
@@ -295,13 +307,26 @@ $('.addSite').on('click', function () {
     url = 'https://' + url;
   }
 
-  var path = require('./assets/img/icon/icon5.png'); //不能使用变量，待优化
-
+  var iconArr = {
+    icon5: require("./assets/img/icon/icon5.png"),
+    icon6: require("./assets/img/icon/icon6.png"),
+    icon7: require("./assets/img/icon/icon7.png"),
+    icon8: require("./assets/img/icon/icon8.png"),
+    icon9: require("./assets/img/icon/icon9.png"),
+    icon10: require("./assets/img/icon/icon10.png")
+  };
+  var path = iconArr["icon".concat(hashMap.length + 1)]; // TODO 依然待优化
 
   hashMap.push({
     logoPath: path,
     url: url
   });
+
+  if (hashMap.length >= 10) {
+    $addSiteLi.css('visibility', 'hidden');
+    alert('真是个贪心的小傻瓜呢~ 😏');
+  }
+
   render(); //重新渲染
 });
 var wallpaperFlag = parseInt(localStorage.getItem("backgroundImageFlag")) || 0; // 标记当前背景图片
@@ -320,12 +345,14 @@ var wallpaperArray = [{
   imagePath: require("./assets/img/wallpaper/plum.jpg")
 }]; // 渲染前先获取 localstorage 中标记的壁纸图片
 
-$navigatorPage.css("backgroundImage", "url(".concat(wallpaperArray[wallpaperFlag].imagePath, ")")); //点击箭头切换背景图片
+$navigatorPage.css("backgroundImage", "url(".concat(wallpaperArray[wallpaperFlag].imagePath, ")")); // 点击箭头切换背景图片
 
 $arrow.on('click', function () {
   $arrow.addClass('rotate');
+  $arrow.css('pointer-events', 'none');
   setTimeout(function () {
     $arrow.removeClass('rotate');
+    $arrow.css('pointer-events', 'auto');
   }, 2000);
   wallpaperFlag = wallpaperFlag === 5 ? 0 : wallpaperFlag += 1;
   localStorage.setItem("backgroundImageFlag", wallpaperFlag); // 存储当前壁纸标记到 localStorage
@@ -341,8 +368,7 @@ Array.from($indicatorLis).forEach(function (item, index) {
 $indicatorUl.on('click', function (event) {
   var clickedIndex = Array.from($indicatorLis).indexOf(event.target);
   if (clickedIndex === -1) return;
-  $indicatorLis.eq(currentIndicator).removeClass('active');
-  $indicatorLis.eq(clickedIndex).addClass('active');
+  $indicatorLis.eq(clickedIndex).addClass('active').siblings().removeClass("active");
   currentIndicator = clickedIndex;
   $navigatorPage.css('margin-top', "".concat(clickedIndex * -100, "vh"));
 }); // 监听鼠标滚轮 切换屏幕 0：导航 1：照片墙
@@ -356,9 +382,8 @@ $(document).on("mousewheel DOMMouseScroll", function (event) {
     currentIndicator--;
 
     if (currentIndicator >= 0) {
-      $indicatorLis.eq(currentIndicator + 1).removeClass('active');
       $navigatorPage.css('margin-top', "".concat(-currentIndicator * 100, "vh"));
-      $indicatorLis.eq(currentIndicator).addClass('active');
+      $indicatorLis.eq(currentIndicator).addClass('active').siblings().removeClass("active");
     } else {
       currentIndicator = 0;
     }
@@ -367,9 +392,8 @@ $(document).on("mousewheel DOMMouseScroll", function (event) {
     currentIndicator++;
 
     if (currentIndicator <= $indicatorLis.length - 1) {
-      $indicatorLis.eq(currentIndicator - 1).removeClass('active');
       $navigatorPage.css('margin-top', "".concat(-currentIndicator * 100, "vh"));
-      $indicatorLis.eq(currentIndicator).addClass('active');
+      $indicatorLis.eq(currentIndicator).addClass('active').siblings().removeClass("active");
     } else {
       currentIndicator = $indicatorLis.length - 1;
     }
@@ -381,8 +405,8 @@ window.onbeforeunload = function () {
   localStorage.setItem('sitesCache', newSitesCache);
 }; // 监听键盘事件
 // $(document).on('keypress', (event) => {
-//   const key = event.key; //const {key} = event;
-//   for (let i = 0; i < hashMap.length; i++) {
+//   const key = event.key //const {key} = event
+//   for (let i = 0 i < hashMap.length i++) {
 //     if (hashMap[i].logo.toLowerCase() === key) {
 //       window.open(hashMap[i].url)
 //     }
@@ -399,5 +423,5 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-},{"./assets/img/icon/icon1.png":"AB3p","./assets/img/icon/icon2.png":"GXZF","./assets/img/icon/icon3.png":"Skyo","./assets/img/icon/icon4.png":"sTvj","./assets/img/icon/icon5.png":"f9I4","./assets/img/wallpaper/yourname.jpg":"kAkt","./assets/img/wallpaper/lantern.jpg":"UonK","./assets/img/wallpaper/bike.jpg":"SLsw","./assets/img/wallpaper/alley.png":"O0r3","./assets/img/wallpaper/pier.png":"C48A","./assets/img/wallpaper/plum.jpg":"ZaH3"}]},{},["epB2"], null)
-//# sourceMappingURL=main.d66bdf26.js.map
+},{"./assets/img/icon/icon1.png":"AB3p","./assets/img/icon/icon2.png":"GXZF","./assets/img/icon/icon3.png":"Skyo","./assets/img/icon/icon4.png":"sTvj","./assets/img/icon/icon5.png":"f9I4","./assets/img/icon/icon6.png":"TLFA","./assets/img/icon/icon7.png":"aS9c","./assets/img/icon/icon8.png":"EsZT","./assets/img/icon/icon9.png":"WTB7","./assets/img/icon/icon10.png":"jYai","./assets/img/wallpaper/yourname.jpg":"kAkt","./assets/img/wallpaper/lantern.jpg":"UonK","./assets/img/wallpaper/bike.jpg":"SLsw","./assets/img/wallpaper/alley.png":"O0r3","./assets/img/wallpaper/pier.png":"C48A","./assets/img/wallpaper/plum.jpg":"ZaH3"}]},{},["epB2"], null)
+//# sourceMappingURL=main.628c5b98.js.map
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 // 获取localStorage - 网址
-let oldSitesCache = localStorage.getItem('sitesCache');
-let sitesCache = JSON.parse(oldSitesCache);
+let oldSitesCache = localStorage.getItem('sitesCache')
+let sitesCache = JSON.parse(oldSitesCache)
 let hashMap = sitesCache ||  [
-  {logoPath:require(`./assets/img/icon/icon1.png`),url:'http://www.graduate.nuaa.edu.cn'},
-  {logoPath:require(`./assets/img/icon/icon2.png`),url:'https://juejin.im'},
-  {logoPath:require(`./assets/img/icon/icon3.png`),url:'https://modao.cc'},
-  {logoPath:require(`./assets/img/icon/icon4.png`),url:'https://www.cnki.net'}
+  {logoPath: require(`./assets/img/icon/icon1.png`), url: 'http://www.graduate.nuaa.edu.cn'},
+  {logoPath: require(`./assets/img/icon/icon2.png`), url: 'https://juejin.im'},
+  {logoPath: require(`./assets/img/icon/icon3.png`), url: 'https://modao.cc'},
+  {logoPath: require(`./assets/img/icon/icon4.png`), url: 'https://www.cnki.net'}
 ]
 
 // 获取localStorage - notes
-let oldNotesCache = localStorage.getItem('notesCache');
-let notesCache = JSON.parse(oldNotesCache);
-let notesArray = notesCache || ['你还是一个没有愿望的小傻瓜呢 ~'];
+let oldNotesCache = localStorage.getItem('notesCache')
+let notesCache = JSON.parse(oldNotesCache)
+let notesArray = notesCache || ['你还是一个没有愿望的小傻瓜呢 ~']
 
 // 简化 url
 const simplifyUrl = (url) => {
@@ -19,50 +19,50 @@ const simplifyUrl = (url) => {
     .replace('http://', '')
     .replace('www.', '')
     .replace('graduate.', '')
-    .replace(/\/.*/, ''); // 删除 / 开头的内容
+    .replace(/\/.*/, '') // 删除 / 开头的内容
 }
 
-let $navigatorPage = $('.navigator-page');  // 导航页
-let $picPage = $('.pic-page');  // 照片墙
-let $indicatorUl = $('.indicator ul');  // 切换标识 ul
-let $indicatorLis = $indicatorUl.find('li');  // 切换标识 ul > lis
-let $notesInput = $('.notesInput'); // 获取便签 input
-let $notesList = $('.notesList'); // 获取便签 list
-let $notesListUl = $('.notesListUl'); // 获取便签 list
-let $notesButton = $('.notesButton'); // 获取便签按钮
-let $tabBar = $('.tab-bar');  // 获取 tabBar 的按钮
-let $search = $('.search');  // 获取 search 表单
-let $input = $('.search input');  // 获取 search 表单的 input
-let $addSiteLi = $('.addSiteLi'); // 获取新增快捷方式按钮
-let $arrow = $('.arrow'); // 获取底部的箭头
+let $navigatorPage = $('.navigator-page')  // 导航页
+let $picPage = $('.pic-page')  // 照片墙
+let $indicatorUl = $('.indicator ul')  // 切换标识 ul
+let $indicatorLis = $indicatorUl.find('li')  // 切换标识 ul > lis
+let $notesInput = $('.notesInput') // 获取便签 input
+let $notesList = $('.notesList') // 获取便签 list
+let $notesListUl = $('.notesListUl') // 获取便签 list
+let $notesButton = $('.notesButton') // 获取便签按钮
+let $tabBar = $('.tab-bar')  // 获取 tabBar 的按钮
+let $search = $('.search')  // 获取 search 表单
+let $input = $('.search input')  // 获取 search 表单的 input
+let $addSiteLi = $('.addSiteLi') // 获取新增快捷方式按钮
+let $arrow = $('.arrow') // 获取底部的箭头
 
 // focus 许愿 input 显示愿望清单
 $notesInput.on('focus',() => {
-  $notesList.addClass('showNotes');
+  $notesList.addClass('showNotes')
 })
 $notesInput.on('blur',() => {
-  $notesList.removeClass('showNotes');
+  $notesList.removeClass('showNotes')
 })
 $notesButton.on('click', () => {
   if ($notesInput.val() === '') {
-    alert('你还没写愿望呢，小傻瓜！');
+    alert('你还没写愿望呢，小傻瓜！')
   } else {
     $notesListUl.append(`
       <li>
-          <span style="line-height: 40px; max-width: 280px;">${$notesInput.val()}</span>
+          <span style="line-height: 40px max-width: 280px">${$notesInput.val()}</span>
           <svg class="icon"><use xlink:href="#icon-aixin"></use></svg>
         </li>
-    `);
+    `)
 
-    notesArray.push($notesInput.val());
+    notesArray.push($notesInput.val())
     if (notesArray.length > 1 && notesArray[0] === '您目前还没有梦想呢，小傻瓜 ~') {
-      notesArray = notesArray.splice(1);
+      notesArray = notesArray.splice(1)
     }
-    let newNotesCache = JSON.stringify(notesArray);
-    localStorage.setItem('notesCache',newNotesCache);
-    alert('当当当当，许愿成功啦！');
-    $notesInput.val('');
-    render();
+    let newNotesCache = JSON.stringify(notesArray)
+    localStorage.setItem('notesCache',newNotesCache)
+    alert('当当当当，许愿成功啦！')
+    $notesInput.val('')
+    render()
   }
 })
 $notesInput.bind('keypress', (event) => {
@@ -71,48 +71,48 @@ $notesInput.bind('keypress', (event) => {
   }
 })
 
-$tabBar.on('click',"div",(event) => { // tabBar事件委托
-  const $tabItem = $(event.currentTarget);  //获取当前被点击的元素
-  $tabItem.addClass("selected").siblings().removeClass("selected"); // toggleClass(value, stateVal); link mdn
+$tabBar.on('click', "div", (event) => { // tabBar事件委托
+  const $tabItem = $(event.currentTarget)  //获取当前被点击的元素
+  $tabItem.addClass("selected").siblings().removeClass("selected") // toggleClass(value, stateVal) link mdn
 
-  let index = $tabItem.index();
+  let index = $tabItem.index()
 
   // 采取四个表单的措施也是可以的，或者采用判断 index 值修改表单的action和input的name属性
-  // $tabContent.children().eq(index).addClass("active").siblings().removeClass("active");
+  // $tabContent.children().eq(index).addClass("active").siblings().removeClass("active")
   if (index === 0){
-    $search.attr("action","http://www.baidu.com/s");
-    $input.attr("name","wd");
-    $input.attr("placeholder","众里寻他千百度 —— 百度");
+    $search.attr("action","http://www.baidu.com/s")
+    $input.attr("name","wd")
+    $input.attr("placeholder","众里寻他千百度 —— 百度")
   }else if (index === 1){
-    $search.attr("action","https://www.zhihu.com/search");
-    $input.attr("name","");
-    $input.attr("placeholder","我们都是有问题的人 —— 知乎");
+    $search.attr("action","https://www.zhihu.com/search")
+    $input.attr("name","")
+    $input.attr("placeholder","我们都是有问题的人 —— 知乎")
   }else if (index === 2){
-    $search.attr("action","https://so.csdn.net/so/search/s.do");
-    $input.attr("name","q");
-    $input.attr("placeholder","成就一亿技术人 —— CSDN");
+    $search.attr("action","https://so.csdn.net/so/search/s.do")
+    $input.attr("name","q")
+    $input.attr("placeholder","成就一亿技术人 —— CSDN")
   }else if(index === 3){
-    $search.attr("action","https://github.com/search");
-    $input.attr("name","q");
-    $input.attr("placeholder","创建自己的开源项目 — GitHub");
+    $search.attr("action","https://github.com/search")
+    $input.attr("name","q")
+    $input.attr("placeholder","创建自己的开源项目 — GitHub")
   }else if(index === 4){
-    $search.attr("action","http://ss.chaoxing.com/search");// 超新星
-    $input.attr("name","sw");
-    $input.attr("placeholder","超星发现 —— 小傻瓜加油！");
+    $search.attr("action","http://ss.chaoxing.com/search")// 超新星
+    $input.attr("name","sw")
+    $input.attr("placeholder","超星发现 —— 小傻瓜加油！")
   }else if (index === 5){
-    $search.attr("action","http://xueshu.baidu.com/s");// 百度学术
-    $input.attr("name","wd");
-    $input.attr("placeholder","保持学习的态度 —— 百度学术");
+    $search.attr("action","http://xueshu.baidu.com/s")// 百度学术
+    $input.attr("name","wd")
+    $input.attr("placeholder","保持学习的态度 —— 百度学术")
   }else{
-    $search.attr("action","https://book.duxiu.com/search");// 读秀
-    $input.attr("name","sw");
-    $input.attr("placeholder","海量学术文献搜索 —— 读秀");
+    $search.attr("action","https://book.duxiu.com/search")// 读秀
+    $input.attr("name","sw")
+    $input.attr("placeholder","海量学术文献搜索 —— 读秀")
   }
 })
 
 // 页面渲染render
 let render = function(){
-  $('.siteList').find('li:not(.addSiteLi)').remove(); // 渲染前移除添加按钮前的模块
+  $('.siteList').find('li:not(.addSiteLi)').remove() // 渲染前移除添加按钮前的模块
   hashMap.forEach((item,index)=>{ // 根据hashMap创建相应的元素并添加到新增按钮前
     let $li = $(`<li class="block">
       <a href="${item.url}">
@@ -121,52 +121,61 @@ let render = function(){
       </a>
       <div class="close">×</div>
     </li>
-    `).insertBefore($addSiteLi);
+    `).insertBefore($addSiteLi)
 
     if (hashMap.length >= 10)
       $addSiteLi.css('visibility', 'hidden')
 
     $li.on('click','.close',(event) => {
-      event.stopPropagation();  // 阻止事件冒泡
-      hashMap.splice(index,1);
+      event.stopPropagation()  // 阻止事件冒泡
+      hashMap.splice(index,1)
       $addSiteLi.css('visibility', 'visible')
-      render();
+      render()
     })
   })
 
-  $notesListUl.find('li').remove(); //渲染前移除之前的notes
+  $notesListUl.find('li').remove() //渲染前移除之前的notes
   notesArray.forEach((item) => {
     let $li = $(`<li title="点击小红心就表示愿望已经实现了哦 ~">
-      <span style="line-height: 40px; max-width: 280px;">${item}</span>
+      <span style="line-height: 40px max-width: 280px">${item}</span>
       <svg class="icon" onclick="window.alert('愿望实现功能正在开发当中呢 ~')"><use xlink:href="#icon-aixin"></use></svg>
     </li>
-    `);
+    `)
 
-    $notesListUl.append($li);
+    $notesListUl.append($li)
   })
 }
 
 // 页面刷新时先渲染 hashMap
-render();
+render()
 
 // 点击添加快捷方式按钮，添加相应的 li 网址模块
-$('.addSite').on('click',() => {
-  let url = window.prompt('请输入你要访问的网址！');
+$('.addSite').on('click', () => {
+  let url = window.prompt('请输入你要访问的网址！')
   if (url.indexOf('http') !== 0){
-    url = 'https://' + url;
+    url = 'https://' + url
   }
-  let path = require('./assets/img/icon/icon5.png'); //不能使用变量，待优化
-  hashMap.push({logoPath: path,url: url});
+
+  let iconArr = {
+    icon5: require(`./assets/img/icon/icon5.png`),
+    icon6: require(`./assets/img/icon/icon6.png`),
+    icon7: require(`./assets/img/icon/icon7.png`),
+    icon8: require(`./assets/img/icon/icon8.png`),
+    icon9: require(`./assets/img/icon/icon9.png`),
+    icon10: require(`./assets/img/icon/icon10.png`)
+  }
+  let path = iconArr[`icon${hashMap.length+1}`] // TODO 依然待优化
+  hashMap.push({logoPath: path, url: url})
 
   if (hashMap.length >= 10) {
     $addSiteLi.css('visibility', 'hidden')
     alert('真是个贪心的小傻瓜呢~ 😏')
   }
 
-  render(); //重新渲染
+  render() //重新渲染
 })
 
-let wallpaperFlag = parseInt(localStorage.getItem("backgroundImageFlag")) || 0;  // 标记当前背景图片
+let wallpaperFlag = parseInt(localStorage.getItem("backgroundImageFlag")) || 0  // 标记当前背景图片
 let wallpaperArray = [
   {imagePath: require(`./assets/img/wallpaper/yourname.jpg`)},
   {imagePath: require(`./assets/img/wallpaper/lantern.jpg`)},
@@ -177,7 +186,7 @@ let wallpaperArray = [
 ]
 
 // 渲染前先获取 localstorage 中标记的壁纸图片
-$navigatorPage.css("backgroundImage",`url(${wallpaperArray[wallpaperFlag].imagePath})`);
+$navigatorPage.css("backgroundImage",`url(${wallpaperArray[wallpaperFlag].imagePath})`)
 // 点击箭头切换背景图片
 $arrow.on('click',() => {
   $arrow.addClass('rotate')
@@ -187,8 +196,8 @@ $arrow.on('click',() => {
     $arrow.css('pointer-events', 'auto')
   }, 2000)
 
-  wallpaperFlag = wallpaperFlag === 5 ? 0 : wallpaperFlag += 1;
-  localStorage.setItem("backgroundImageFlag",wallpaperFlag);  // 存储当前壁纸标记到 localStorage
+  wallpaperFlag = wallpaperFlag === 5 ? 0 : wallpaperFlag += 1
+  localStorage.setItem("backgroundImageFlag",wallpaperFlag)  // 存储当前壁纸标记到 localStorage
   $navigatorPage.css("backgroundImage",`url(${wallpaperArray[wallpaperFlag].imagePath})`)
 })
 
@@ -203,7 +212,7 @@ Array.from($indicatorLis).forEach((item, index) => {
 $indicatorUl.on('click', (event) => {
   let clickedIndex = Array.from($indicatorLis).indexOf(event.target)
   if (clickedIndex === -1) return
-  $indicatorLis.eq(clickedIndex).addClass('active').siblings().removeClass("active");
+  $indicatorLis.eq(clickedIndex).addClass('active').siblings().removeClass("active")
   currentIndicator = clickedIndex
   $navigatorPage.css('margin-top', `${clickedIndex * -100}vh`)
 })
@@ -211,13 +220,13 @@ $indicatorUl.on('click', (event) => {
 // 监听鼠标滚轮 切换屏幕 0：导航 1：照片墙
 $(document).on("mousewheel DOMMouseScroll", function (event) {
   var delta = (event.originalEvent.wheelDelta && (event.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
-      (event.originalEvent.detail && (event.originalEvent.detail > 0 ? -1 : 1));              // firefox
+      (event.originalEvent.detail && (event.originalEvent.detail > 0 ? -1 : 1))              // firefox
 
   if (delta > 0) {  // 向上滚
     currentIndicator--
     if (currentIndicator >= 0) {
       $navigatorPage.css('margin-top', `${-currentIndicator * 100}vh`)
-      $indicatorLis.eq(currentIndicator).addClass('active').siblings().removeClass("active");
+      $indicatorLis.eq(currentIndicator).addClass('active').siblings().removeClass("active")
     } else {
       currentIndicator = 0
     }
@@ -225,24 +234,24 @@ $(document).on("mousewheel DOMMouseScroll", function (event) {
     currentIndicator++
     if (currentIndicator <= $indicatorLis.length - 1) {
       $navigatorPage.css('margin-top', `${-currentIndicator * 100}vh`)
-      $indicatorLis.eq(currentIndicator).addClass('active').siblings().removeClass("active");
+      $indicatorLis.eq(currentIndicator).addClass('active').siblings().removeClass("active")
     } else {
       currentIndicator = $indicatorLis.length - 1
     }
   }
-});
+})
 
 
 // 窗口关闭前保存到localStorage
 window.onbeforeunload = function () {
-  let newSitesCache = JSON.stringify(hashMap);
-  localStorage.setItem('sitesCache',newSitesCache);
+  let newSitesCache = JSON.stringify(hashMap)
+  localStorage.setItem('sitesCache',newSitesCache)
 }
 
 // 监听键盘事件
 // $(document).on('keypress', (event) => {
-//   const key = event.key; //const {key} = event;
-//   for (let i = 0; i < hashMap.length; i++) {
+//   const key = event.key //const {key} = event
+//   for (let i = 0 i < hashMap.length i++) {
 //     if (hashMap[i].logo.toLowerCase() === key) {
 //       window.open(hashMap[i].url)
 //     }
